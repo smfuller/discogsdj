9b3de7c18b7b2ffa0afcb6bb8d0cd4e1559da0eb
Steve Fuller <steve@palescale.com>
2021-02-14 00:25:33 -0700

    split processes into general functions and handle pagination

--- src/main/kotlin/dj.kt
+++ src/main/kotlin/dj.kt
@@ -10,15 +10,36 @@ val DISCOGS_CONSUMER_KEY: String = System.getenv("DISCOGS_CONSUMER_KEY")
 val DISCOGS_CONSUMER_SECRET: String = System.getenv("DISCOGS_CONSUMER_SECRET")
 val DISCOGS_MY_ACCESS_TOKEN: String = System.getenv("DISCOGS_MY_ACCESS_TOKEN")
 val DISCOGS_MY_ACCESS_SECRET: String = System.getenv("DISCOGS_MY_ACCESS_SECRET")
+val client: HttpClient = HttpClient.newHttpClient()
 
 fun main() {
 
     print("Verify your username to get your collection >> ")
     val username = readLine()
+    val wantsURI = "https://api.discogs.com/users/${username}/wants?per_page=30"
+    val collectionURI = "https://api.discogs.com/users/${username}/collection"
 
-    val client = HttpClient.newHttpClient()
+    val fullJson = arrayListOf<JsonObject>()
+
+    val wantsJson = getJson(client, wantsURI)
+    val collectionJson = getJson(client, collectionURI)
+
+    val tracklist = collectionJson.lookup<String>(
+        "releases.basic_information.tracklist.title"
+    )
+
+    // pagination testing
+    getNextPageJson(wantsJson, fullJson)
+    for(i in fullJson) {
+        i.lookup<Int>(
+            "wants.id"
+        ).let { for(j in it) println(j) }
+    }
+}
+
+fun getJson(client: HttpClient, uri: String): JsonObject {
     val request = HttpRequest
-        .newBuilder(URI.create("https://api.discogs.com/users/${username}/collection"))
+        .newBuilder(URI.create(uri))
         .header("accept", "application.json")
         .header("Authorization",
             "OAuth oauth_consumer_key=\"${DISCOGS_CONSUMER_KEY}\"," +
@@ -28,20 +49,22 @@ fun main() {
         .build()
     val response = client.send(request, HttpResponse.BodyHandlers.ofString());
     val responseStream: InputStream = response.body().byteInputStream()
-    val json = Parser.default().parse(responseStream) as JsonObject
-    val tracklist = json.lookup<String>(
-        "releases.basic_information.tracklist.title"
+
+    return Parser.default().parse(responseStream) as JsonObject
+}
+
+fun getNextPageJson(json: JsonObject, list: ArrayList<JsonObject>): ArrayList<JsonObject> {
+    println("Adding a json object...")
+    list.add(json)
+    val nextPage = json.lookup<String?>(
+        "pagination.urls.next"
     )
-    val iterate = tracklist.listIterator()
-    while(iterate.hasNext()) {
-        val i = iterate.next()
-        if(i.startsWith("The "))
-            iterate.set(i.substring(startIndex = 4).plus(", The "))
+
+    // keep going if there are more pages
+    nextPage.value[0]?.let {
+        getNextPageJson(getJson(client, it), list)
     }
 
-    tracklist.sort()
-    for(i in tracklist.distinct()) {
-        println(i)
-    }
+    return list
 }
 

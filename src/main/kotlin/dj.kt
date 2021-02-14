95c89d8e201a3708d30906b355835fa30cca73bc
Steve Fuller <steve@palescale.com>
2021-02-13 21:13:44 -0700

    we got real api json now boys

--- src/main/kotlin/dj.kt
+++ src/main/kotlin/dj.kt
@@ -1,25 +1,46 @@
 import com.beust.klaxon.*
-import java.io.File
+import java.io.InputStream
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+
+val DISCOGS_CONSUMER_KEY: String = System.getenv("DISCOGS_CONSUMER_KEY")
+val DISCOGS_CONSUMER_SECRET: String = System.getenv("DISCOGS_CONSUMER_SECRET")
+val DISCOGS_MY_ACCESS_TOKEN: String = System.getenv("DISCOGS_MY_ACCESS_TOKEN")
+val DISCOGS_MY_ACCESS_SECRET: String = System.getenv("DISCOGS_MY_ACCESS_SECRET")
 
 fun main() {
-    val test = Parser.default()
-        .parse(File("wow.json").bufferedReader()) as JsonObject
 
-    val array = test.lookup<String>(
+    print("Verify your username to get your collection >> ")
+    val username = readLine()
+
+    val client = HttpClient.newHttpClient()
+    val request = HttpRequest
+        .newBuilder(URI.create("https://api.discogs.com/users/${username}/collection"))
+        .header("accept", "application.json")
+        .header("Authorization",
+            "OAuth oauth_consumer_key=\"${DISCOGS_CONSUMER_KEY}\"," +
+                    "oauth_token=\"${DISCOGS_MY_ACCESS_TOKEN}\"," +
+                    "oauth_signature_method=\"HMAC-SHA1\"," +
+                    "oauth_version=\"1.0\"")
+        .build()
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    val responseStream: InputStream = response.body().byteInputStream()
+    val json = Parser.default().parse(responseStream) as JsonObject
+    val tracklist = json.lookup<String>(
         "releases.basic_information.tracklist.title"
     )
-
-    val iterate = array.listIterator()
-
+    val iterate = tracklist.listIterator()
     while(iterate.hasNext()) {
         val i = iterate.next()
         if(i.startsWith("The "))
             iterate.set(i.substring(startIndex = 4).plus(", The "))
     }
 
-    array.sort()
-
-    for(i in array.distinct()) {
+    tracklist.sort()
+    for(i in tracklist.distinct()) {
         println(i)
     }
 }

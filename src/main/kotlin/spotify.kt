b5ea31493191b725d00085082ca564c0e808aebb
Steve Fuller <steve@palescale.com>
2021-02-28 00:54:18 -0700

    get all auth loaded for Spotify API and cleanup

--- src/main/kotlin/spotify.kt
+++ src/main/kotlin/spotify.kt
@@ -1,19 +1,20 @@
-import com.beust.klaxon.JsonObject
-import com.beust.klaxon.Parser
+import com.beust.klaxon.*
 import java.io.InputStream
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-val TOKEN: String = System.getenv("SPOTIFY_MY_ACCESS_TOKEN")
+
+val SPOTIFY_CLIENT_ID: String = System.getenv("SPOTIFY_CLIENT_ID")
+val SPOTIFY_CLIENT_SECRET: String = System.getenv("SPOTIFY_CLIENT_SECRET")
+val SPOTIFY_MY_BEARER_TOKEN = System.getenv("SPOTIFY_MY_BEARER_TOKEN")
+val SPOTIFY_REFRESH_TOKEN: String = System.getenv("SPOTIFY_REFRESH_TOKEN")
 val spotifyClient: HttpClient = HttpClient.newHttpClient()
 
 fun main() {
     val spotifyURI = "https://api.spotify.com/v1/tracks?ids=3n3Ppam7vgaVa1iaRUc9Lp%2C3twNvmDtFQtAd5gMKedhLD"
-    print(getSpotifyJson(spotifyClient, spotifyURI).toJsonString(true))
-
-
+    val spotifyJson = getSpotifyJson(spotifyClient, spotifyURI)
 }
 
 fun getSpotifyJson(client: HttpClient, uri: String): JsonObject {
@@ -21,7 +22,7 @@ fun getSpotifyJson(client: HttpClient, uri: String): JsonObject {
         .newBuilder(URI.create(uri))
         .header("accept", "application.json")
         .header("Authorization",
-            "Bearer $TOKEN")
+            "Bearer $SPOTIFY_MY_BEARER_TOKEN")
         .build()
     val response = client.send(request, HttpResponse.BodyHandlers.ofString());
     val responseStream: InputStream = response.body().byteInputStream()

ad81c04c4119582fbd520ae792544207c6adfa83
Steve Fuller <steve@palescale.com>
2021-02-28 01:53:16 -0700

    can create Spotify playlists

--- src/main/kotlin/spotify.kt
+++ src/main/kotlin/spotify.kt
@@ -4,17 +4,21 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 val SPOTIFY_CLIENT_ID: String = System.getenv("SPOTIFY_CLIENT_ID")
 val SPOTIFY_CLIENT_SECRET: String = System.getenv("SPOTIFY_CLIENT_SECRET")
 val SPOTIFY_MY_BEARER_TOKEN: String = System.getenv("SPOTIFY_MY_BEARER_TOKEN")
 val SPOTIFY_REFRESH_TOKEN: String = System.getenv("SPOTIFY_REFRESH_TOKEN")
+val SPOTIFY_USERNAME: String = System.getenv("SPOTIFY_USERNAME")
 val spotifyClient: HttpClient = HttpClient.newHttpClient()
 
 fun main() {
-    val spotifyURI = "https://api.spotify.com/v1/tracks?ids=3n3Ppam7vgaVa1iaRUc9Lp%2C3twNvmDtFQtAd5gMKedhLD"
-    val spotifyJson = getSpotifyJson(spotifyClient, spotifyURI)
+    val spotifyURI = "https://api.spotify.com/v1/users/$SPOTIFY_USERNAME/playlists"
+
+    println(createPlaylist(spotifyClient, spotifyURI))
 }
 
 fun getSpotifyJson(client: HttpClient, uri: String): JsonObject {
@@ -28,4 +32,27 @@ fun getSpotifyJson(client: HttpClient, uri: String): JsonObject {
     val responseStream: InputStream = response.body().byteInputStream()
 
     return Parser.default().parse(responseStream) as JsonObject
+}
+
+fun createPlaylist(client: HttpClient, uri: String): String {
+    val now = LocalDateTime.now().format(DateTimeFormatter
+        .ofPattern("yyyy-MM-dd hh:mm:ss a"))
+
+    val playlistJson = """
+        {
+            "name": "DiscogsDJ - $now",
+            "description": "This is a playlist",
+            "public": false
+        }
+    """.trimIndent()
+    val request = HttpRequest
+        .newBuilder(URI.create(uri))
+        .header("accept", "application.json")
+        .header("Authorization",
+            "Bearer $SPOTIFY_MY_BEARER_TOKEN")
+        .POST(HttpRequest.BodyPublishers.ofString(playlistJson))
+        .build()
+
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    return response.body()
 }
